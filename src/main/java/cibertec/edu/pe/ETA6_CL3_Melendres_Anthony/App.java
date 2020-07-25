c586a0205ed3ed8c89817d041d91375faaa52eff
Sistemas y Soporte <sistemas@prcp.com.pe>
2020-07-25 15:47:25 -0500

    Examen_CL3_Vista

---
+++ src/main/java/cibertec/edu/pe/ETA6_CL3_Melendres_Anthony/App.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.ETA6_CL3_Melendres_Anthony;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
